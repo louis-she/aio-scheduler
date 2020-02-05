f7512c33de940a4914b82c5ce190a23358f02901
chenglu <chenglu.she@gmail.com>
2020-02-05 13:01:36 +0800

    add perform later

--- aio_scheduler/job.py
+++ aio_scheduler/job.py
@@ -2,7 +2,7 @@ import asyncio
 import importlib
 import json
 from uuid import uuid4
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from .utils import maybe_future
 
@@ -20,6 +20,18 @@ class Job:
         self.arguments = arguments
         await self.adapter.enqueue(
             activate_at=int(datetime.timestamp(at)),
+            encode=self.serialize())
+
+    async def perform_later(
+            self, days=0, seconds=0, microseconds=0,
+            milliseconds=0, minutes=0, hours=0, weeks=0, **arguments):
+        delta = timedelta(
+            days=days, seconds=seconds, microseconds=microseconds,
+            milliseconds=milliseconds, minutes=minutes, hours=hours,
+            weeks=weeks)
+        self.arguments = arguments
+        await self.adapter.enqueue(
+            activate_at=int(datetime.timestamp(datetime.now() + delta)),
             encode=self.serialize())
 
     @classmethod
@@ -50,8 +62,12 @@ class Job:
         try:
             await maybe_future(self.perform(**self.arguments))
             self.logger.info(f'Job.{self.uuid} seccessfully processed')
-        except Exception:
+        except Exception as e:
             self.logger.error(f'Failed to process Job.{self.uuid}', exc_info=True)
+            await maybe_future(self.handle_exception(e))
+
+    async def handle_exception(self, e):
+        pass
 
     def perform(self):
         raise NotImplementedError
